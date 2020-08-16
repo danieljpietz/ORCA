63929a4b11d491a829d18dc1fa2f34d413ef276a
danieljpietz <danieljpietz@gmail.com>
2020-08-15 23:51:20 -0700

    Added equality operator

--- ORCAMath/Complex.h
+++ ORCAMath/Complex.h
@@ -28,124 +28,124 @@ namespace ORCA {
  * Stores numbers in the form a+bi where i = sqrt(-1)
  * @tparam T Complex base type
  */
-    template <class T>
-    class Complex {
-    protected:
-        T _real;    // Real component of complex number
-        T _imag;    // Imaginary component of complex number
-    public:
-
-        /**
-         * Default constructor for Complex class. Both components are set to 0
-         */
-
-        Complex() {
-            this->_real = 0;
-            this->_imag = 0;
-        } /* Complex() */
-
-        /**
-         * Casted constructor from std::initializer_list.
-         * Real and imaginary components are set to respective values in list
-         * An ORCA_BAD_DIMENSIONS exception is thrown should the length of the list not equal 2
-         */
-
-        Complex(std::initializer_list<T> _castValues) {
+template <class T>
+class Complex {
+protected:
+    T _real;    // Real component of complex number
+    T _imag;    // Imaginary component of complex number
+public:
+    
+    /**
+     * Default constructor for Complex class. Both components are set to 0
+     */
+    
+    Complex() {
+        this->_real = 0;
+        this->_imag = 0;
+    } /* Complex() */
+    
+    /**
+     * Casted constructor from std::initializer_list.
+     * Real and imaginary components are set to respective values in list
+     * An ORCA_BAD_DIMENSIONS exception is thrown should the length of the list not equal 2
+     */
+    
+    Complex(std::initializer_list<T> _castValues) {
 #ifndef ORCA_DISABLE_DIMENSIONS_CHECKS
-            if (_castValues.size() > 2) {
-                throw ORCAExcept::BadDimensionsError();
-            }
+        if (_castValues.size() > 2) {
+            throw ORCAExcept::BadDimensionsError();
+        }
 #endif
-            this->_real = *_castValues.begin();
-
-            if (_castValues.size() == 2) {
-                this->_imag = *(_castValues.begin() + 1);
-            }
-        } /* Complex(std::initializer_list<T> _castValues) */
-
-        /**
-         * Defined constructor for Complex
-         * @param real Real Component
-         * @param imag Imaginary Component
-         */
-
-        Complex(T real, T imag) {
-            this->_real = real;
-            this->_imag = imag;
-        } /* Complex(T real, T imag) */
-
-        /**
-         * Casted Constructor for Complex Class and non Complex type
-         * @tparam T1 class type of casted parameter
-         * @param cast casted value
-         */
-
-        template<class T1>
-        Complex(T1 cast) {
-            this->_real = cast;
-            this->_imag = 0;
-        } /* Complex(T1 cast) */
-
-        /**
-         * Casted Constructor for Complex class and Complex type
-         * @tparam T1 class type of casted parameter
-         * @param cast casted value
-         */
-
-        template<class T1>
-        Complex(Complex<T1> cast) {
-            this->_real = cast.real();
-            this->_imag = cast.imag();
-        } /* Complex(T1 cast) */
-
-        /* Below are public getters for the Complex class */
-
-        /**
-         * Getter for real component of Complex class
-         * @return real component of value
-         */
-
-        T real() const {
-            return this->_real;
+        this->_real = *_castValues.begin();
+        
+        if (_castValues.size() == 2) {
+            this->_imag = *(_castValues.begin() + 1);
         }
-
-        /**
-         * Getter for real component of Complex class
-         * @return real component of value
-         */
-
-        T re() const {
-            return this->_real;
-        }
-
-        /**
-         * Getter for imaginary component of Complex class
-         * @return imaginary component of value
-         */
-
-        T imag() const {
-            return this->_imag;
-        }
-
-        /**
-         * Getter for real component of Complex class
-         * @return real component of value
-         */
-
-        T im() const {
-            return this->_imag;
-        }
-
-        /**
-         * Conjugate getter of Complex class
-         * @return Conjugate of variable
-         */
-
-        Complex<T> conj() const {
-            return Complex<T>(this->_real, -this->_imag);
-        }
-
-    };
+    } /* Complex(std::initializer_list<T> _castValues) */
+    
+    /**
+     * Defined constructor for Complex
+     * @param real Real Component
+     * @param imag Imaginary Component
+     */
+    
+    Complex(T real, T imag) {
+        this->_real = real;
+        this->_imag = imag;
+    } /* Complex(T real, T imag) */
+    
+    /**
+     * Casted Constructor for Complex Class and non Complex type
+     * @tparam T1 class type of casted parameter
+     * @param cast casted value
+     */
+    
+    template<class T1>
+    Complex(T1 cast) {
+        this->_real = cast;
+        this->_imag = 0;
+    } /* Complex(T1 cast) */
+    
+    /**
+     * Casted Constructor for Complex class and Complex type
+     * @tparam T1 class type of casted parameter
+     * @param cast casted value
+     */
+    
+    template<class T1>
+    Complex(Complex<T1> cast) {
+        this->_real = cast.real();
+        this->_imag = cast.imag();
+    } /* Complex(T1 cast) */
+    
+    /* Below are public getters for the Complex class */
+    
+    /**
+     * Getter for real component of Complex class
+     * @return real component of value
+     */
+    
+    T real() const {
+        return this->_real;
+    }
+    
+    /**
+     * Getter for real component of Complex class
+     * @return real component of value
+     */
+    
+    T re() const {
+        return this->_real;
+    }
+    
+    /**
+     * Getter for imaginary component of Complex class
+     * @return imaginary component of value
+     */
+    
+    T imag() const {
+        return this->_imag;
+    }
+    
+    /**
+     * Getter for real component of Complex class
+     * @return real component of value
+     */
+    
+    T im() const {
+        return this->_imag;
+    }
+    
+    /**
+     * Conjugate getter of Complex class
+     * @return Conjugate of variable
+     */
+    
+    Complex<T> conj() const {
+        return Complex<T>(this->_real, -this->_imag);
+    }
+    
+};
 
 /* Below are all overloaded mathematical operators for the complex class */
 
@@ -158,10 +158,10 @@ namespace ORCA {
  * @return sum of two numbers
  */
 
-    template<class T1, class T2>
-    auto operator + (Complex<T1> c1, Complex<T2> c2) {
-        return Complex<decltype(c1.real() + c2.real())>(c1.real() + c2.real(), c1.imag() + c2.imag());
-    }
+template<class T1, class T2>
+auto operator + (Complex<T1> c1, Complex<T2> c2) {
+    return Complex<decltype(c1.real() + c2.real())>(c1.real() + c2.real(), c1.imag() + c2.imag());
+}
 
 /**
  * Addition operator for a complex and non complex type
@@ -172,10 +172,10 @@ namespace ORCA {
  * @return sum of two numbers
  */
 
-    template<class T1, class T2>
-    auto operator + (Complex<T1> c1, T2 c2) {
-        return c1 + Complex<T2>(c2);
-    }
+template<class T1, class T2>
+auto operator + (Complex<T1> c1, T2 c2) {
+    return c1 + Complex<T2>(c2);
+}
 
 /**
  * Addition operator for a non complex and  complex type
@@ -186,10 +186,10 @@ namespace ORCA {
  * @return sum of two numbers
  */
 
-    template<class T1, class T2>
-    auto operator + (T1 c1, Complex<T2> c2) {
-        return Complex<T1>(c1) + c2;
-    }
+template<class T1, class T2>
+auto operator + (T1 c1, Complex<T2> c2) {
+    return Complex<T1>(c1) + c2;
+}
 
 /**
  * Subtraction operator for 2 complex types
@@ -200,10 +200,10 @@ namespace ORCA {
  * @return difference between numbers
  */
 
-    template<class T1, class T2>
-    auto operator - (Complex<T1> c1, Complex<T2> c2) {
-        return Complex<decltype(c1.real() - c2.real())>(c1.real() - c2.real(), c1.imag() - c2.imag());
-    }
+template<class T1, class T2>
+auto operator - (Complex<T1> c1, Complex<T2> c2) {
+    return Complex<decltype(c1.real() - c2.real())>(c1.real() - c2.real(), c1.imag() - c2.imag());
+}
 
 /**
  * Subtraction operator for a complex and non complex type
@@ -214,10 +214,10 @@ namespace ORCA {
  * @return difference between numbers
  */
 
-    template<class T1, class T2>
-    auto operator - (Complex<T1> c1, T2 c2) {
-        return c1 - Complex<T2>(c2);
-    }
+template<class T1, class T2>
+auto operator - (Complex<T1> c1, T2 c2) {
+    return c1 - Complex<T2>(c2);
+}
 
 /**
  * Subtraction operator for a non complex and  complex type
@@ -228,10 +228,10 @@ namespace ORCA {
  * @return difference between numbers
  */
 
-    template<class T1, class T2>
-    auto operator - (T1 c1, Complex<T2> c2) {
-        return Complex<T1>(c1) - c2;
-    }
+template<class T1, class T2>
+auto operator - (T1 c1, Complex<T2> c2) {
+    return Complex<T1>(c1) - c2;
+}
 
 /**
  * Negation operator for a non complex and  complex type
@@ -240,10 +240,10 @@ namespace ORCA {
  * @return difference between numbers
  */
 
-    template<class T1>
-    auto operator - (Complex<T1> c1) {
-        return Complex<T1>(-c1.real(), -c1.imag());
-    }
+template<class T1>
+auto operator - (Complex<T1> c1) {
+    return Complex<T1>(-c1.real(), -c1.imag());
+}
 
 /**
  * Multiplication operator for 2 complex types
@@ -254,10 +254,10 @@ namespace ORCA {
  * @return product of numbers
  */
 
-    template<class T1, class T2>
-    auto operator * (Complex<T1> c1, Complex<T2> c2) {
-        return Complex<decltype((c1.real() * c2.real()) - (c1.imag() * c2.imag()))>((c1.real() * c2.real()) - (c1.imag() * c2.imag()), (c1.real() * c2.imag()) + (c1.imag() * c2.real()));
-    }
+template<class T1, class T2>
+auto operator * (Complex<T1> c1, Complex<T2> c2) {
+    return Complex<decltype((c1.real() * c2.real()) - (c1.imag() * c2.imag()))>((c1.real() * c2.real()) - (c1.imag() * c2.imag()), (c1.real() * c2.imag()) + (c1.imag() * c2.real()));
+}
 
 /**
  * Multiplication operator for complex and non complex types
@@ -268,10 +268,10 @@ namespace ORCA {
  * @return product of numbers
  */
 
-    template<class T1, class T2>
-    auto operator * (Complex<T1> c1, T2 c2) {
-        return c1 * Complex<T2>(c2);
-    }
+template<class T1, class T2>
+auto operator * (Complex<T1> c1, T2 c2) {
+    return c1 * Complex<T2>(c2);
+}
 
 /**
  * Multiplication operator for non complex and complex types
@@ -282,10 +282,10 @@ namespace ORCA {
  * @return product of numbers
  */
 
-    template<class T1, class T2>
-    auto operator * (T1 c1, Complex<T2> c2) {
-        return Complex<T1>(c1) * c2;
-    }
+template<class T1, class T2>
+auto operator * (T1 c1, Complex<T2> c2) {
+    return Complex<T1>(c1) * c2;
+}
 
 /**
  * Comparison  operator for 2 complex types
@@ -296,10 +296,10 @@ namespace ORCA {
  * @return equalty comparison
  */
 
-    template<class T1, class T2>
-    auto operator == (Complex<T1> c1, Complex<T2> c2) {
-        return (c1.real() == c2.real()) && (c1.imag() == c2.imag());
-    }
+template<class T1, class T2>
+auto operator == (Complex<T1> c1, Complex<T2> c2) {
+    return (c1.real() == c2.real()) && (c1.imag() == c2.imag());
+}
 
 /**
  * Comparison  operator for  complex types and non complex type
@@ -310,10 +310,10 @@ namespace ORCA {
  * @return equalty comparison
  */
 
-    template<class T1, class T2>
-    auto operator == (Complex<T1> c1, T2 c2) {
-        return c1 == Complex<T2>(c2);
-    }
+template<class T1, class T2>
+auto operator == (Complex<T1> c1, T2 c2) {
+    return c1 == Complex<T2>(c2);
+}
 
 /**
  * Comparison  operator for  complex types and non complex type
@@ -324,29 +324,29 @@ namespace ORCA {
  * @return equalty comparison
  */
 
-    template<class T1, class T2>
-    auto operator == (T2 c2, Complex<T1> c1) {
-        return c1 == Complex<T2>(c2);
-    }
+template<class T1, class T2>
+auto operator == (T2 c2, Complex<T1> c1) {
+    return c1 == Complex<T2>(c2);
+}
 
 
 /* Below are other overloaded operators for the Complex class */
 
 /** Overloaded stream by reference */
 
-    template <class T>
-    std::ostream& operator<<(std::ostream& os, const Complex<T>& c) {
-        os << c.re() << " + " << c.im() << ORCA_IMAGINARY_UNIT;
-        return os;
-    }
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Complex<T>& c) {
+    os << c.re() << " + " << c.im() << ORCA_IMAGINARY_UNIT;
+    return os;
+}
 
 /** Overloaded stream by pointer */
 
-    template <class T>
-    std::ostream& operator<<(std::ostream& os, const Complex<T>* c) {
-        os << *c;
-        return os;
-    }
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Complex<T>* c) {
+    os << *c;
+    return os;
+}
 
 
 }
