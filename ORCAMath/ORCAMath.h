0dc9b72d2180458ef7d416461a22df1f70dd3b1c
danieljpietz <danieljpietz@gmail.com>
2020-08-17 23:14:51 -0700

    Added stickyCompute masks

--- ORCAMath/ORCAMath.h
+++ ORCAMath/ORCAMath.h
@@ -10,16 +10,19 @@
 #ifndef ORCAMath_h
 #define ORCAMath_h
 
-#define ORCA_SIZE_TYPE long long
+
+namespace ORCA {
+typedef long long index_t;
+}
 
 /* Error Code Definitions */
 
-#define ORCA_SUCCESS 0x1
-#define ORCA_OUT_OF_BOUNDS 0x2
-#define ORCA_NULL_ARG 0x3
-#define ORCA_EMPTY_ELEMENT 0x4
-#define ORCA_BAD_DIMENSIONS 0x5
-#define ORCA_UNKNOWN_FILL_TYPE 0x6
+#define ORCA_SUCCESS (0x1)
+#define ORCA_OUT_OF_BOUNDS (0x2)
+#define ORCA_NULL_ARG (0x3)
+#define ORCA_EMPTY_ELEMENT (0x4)
+#define ORCA_BAD_DIMENSIONS (0x5)
+#define ORCA_UNKNOWN_FILL_TYPE (0x6)
 
 /* Error Checking Definitions Setup
  * Disable all error checking if ORCA_DISABLE_ERROR_CHECK is defined.
@@ -35,17 +38,20 @@
 
 /* Sticky compute mask definitions */
 
-#define ORCA_DISABLE_STICKY_COMPUTE
 
 #ifndef ORCA_DISABLE_STICKY_COMPUTE
-#define ORCA_STICKY_COMPUTE_DIAG_MASK 1
+#define ORCA_STICKY_COMPUTE_DIAG_MASK (1 << 0)
+#define ORCA_STICKY_COMPUTE_DET_MASK (1 << 1)
 #endif
 
+
+#include "Constants.h"
 #include "Complex.h"
 #include "Quaternion.h"
 #include "Mat.h"
 #include "Vec.h"
 #include "Fill.h"
 #include "Except.h"
+
 
 #endif /* ORCAMath_h */
